cb269dce585cc022341fd3c03a78c6d5cd56d137
Mirek Rusin <mirek@me.com>
2022-10-02 18:25:26 +0200

    chore(filter): updating filter type

--- src/filter.ts
+++ src/filter.ts
@@ -1,8 +1,8 @@
-import type { Predicate, TypePredicate, AsyncPredicate } from './prelude.js'
+import type { Predicate, TypePredicate, AsyncPredicate, Transformer } from './prelude.js'
 
-export function filter<T, U extends T>(predicate: TypePredicate<T, U>): (values: AsyncIterable<T>) => AsyncGenerator<U>
-export function filter<T>(predicate: AsyncPredicate<T>): (values: AsyncIterable<T>) => AsyncGenerator<T>
-export function filter<T>(predicate: Predicate<T>): (values: AsyncIterable<T>) => AsyncGenerator<T>
+export function filter<T, U extends T>(predicate: TypePredicate<T, U>): Transformer<T, U>
+export function filter<T>(predicate: AsyncPredicate<T>): Transformer<T>
+export function filter<T>(predicate: Predicate<T>): Transformer<T>
 
 /** @yields elements passing provided predicate only. */
 export function filter(predicate: (value: unknown, index: number) => unknown) {
