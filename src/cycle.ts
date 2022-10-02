3b8af907bc098e6288b247dd6640d8dba3007f59
Mirek Rusin <mirek@me.com>
2022-10-02 18:24:34 +0200

    feat!(cycle): adding number of repetitions to cycle

--- src/cycle.ts
+++ src/cycle.ts
@@ -1,14 +1,21 @@
-/** @yields infinitely cycled values or nothing if values were empty. */
-export async function* cycle<T>(values: AsyncIterable<T>) {
-  const seen: T[] = []
-  for await (const value of values) {
-    seen.push(value)
-    yield value
+import type { Transformer } from './prelude'
+
+/** @returns transformer cycling through values n times (defaults to infinity). */
+export function cycle<T>(n = Infinity): Transformer<T> {
+  if (n <= 0) {
+    return async function* innerCycleNoop() {}
   }
-  if (seen.length === 0) {
-    return
-  }
-  while (true) {
-    yield* seen
+  return async function* innerCycle(values) {
+    const seen: T[] = []
+    for await (const value of values) {
+      seen.push(value)
+      yield value
+    }
+    if (seen.length === 0) {
+      return
+    }
+    for (let i = 1; i < n; i++) {
+      yield* seen
+    }
   }
 }
