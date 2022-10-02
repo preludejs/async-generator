11ecbbaf92ebe2edcb522fbc475ee41255652bfe
Mirek Rusin <mirek@me.com>
2022-10-02 22:06:35 +0200

    fix: import

--- src/buffered.ts
+++ src/buffered.ts
@@ -1,5 +1,5 @@
 import * as Ch from '@prelude/channel'
-import type { Transformer } from './prelude'
+import type { Transformer } from './prelude.js'
 
 /** @returns transformer that buffers values using provided capacity. */
 export function buffered<T>(cap: number): Transformer<T> {
