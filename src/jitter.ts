18fb47c1201b11854502c6822b3aa1ee9086ea79
Mirek Rusin <mirek@me.com>
2022-10-02 18:23:39 +0200

    feat: adding jitter

---
+++ src/jitter.ts
@@ -0,0 +1,20 @@
+import type { Transformer } from './prelude'
+
+function sleep(milliseconds: number) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+/**
+ * Jitters values in time.
+ *
+ * @param jitter_ The jitter in milliseconds.
+ *
+ * */
+export function jitter<T>(jitter_: number, delay = 0): Transformer<T> {
+  return async function* (values) {
+    for await (const value of values) {
+      await sleep(delay + Math.random() * jitter_)
+      yield value
+    }
+  }
+}
