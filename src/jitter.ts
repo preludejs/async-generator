6c422650b95ef0dbbfd146c1c0c97732d5a33aa1
Mirek Rusin <mirek@me.com>
2023-09-30 07:55:26 +0200

    style: Math.random is safe for jitter

--- src/jitter.ts
+++ src/jitter.ts
@@ -8,7 +8,7 @@ function sleep(milliseconds: number) {
 export function jitter<T>(jitter_: number, delay = 0): Transformer<T> {
   return async function* (values) {
     for await (const value of values) {
-      await sleep(delay + Math.random() * jitter_)
+      await sleep(delay + Math.random() * jitter_) // NOSONAR
       yield value
     }
   }
