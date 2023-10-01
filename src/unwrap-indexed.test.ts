c50c257045fca6704f748691122533a864e13ee7
Mirek Rusin <mirek@me.com>
2023-10-01 11:53:04 +0200

    feat!: renaming `ordered` to `unwrapIndexed`

---
+++ src/unwrap-indexed.test.ts
@@ -0,0 +1,39 @@
+import * as G from './index.js'
+
+test('ordered', async () => {
+  await expect(G.pipe(
+    G.ofIterable([
+      { index: 2, value: 'a' },
+      { index: 0, value: 'b' },
+      { index: 1, value: 'c' }
+    ]),
+    G.unwrapIndexed,
+    G.array,
+  )).resolves.toEqual([
+    'b',
+    'c',
+    'a'
+  ])
+})
+
+test('ordered with invalid 0-based indices', async () => {
+  await expect(G.pipe(
+    G.ofIterable([
+      { index: 1, value: 'a' },
+      { index: 2, value: 'b' },
+      { index: 3, value: 'c' }
+    ]),
+    G.unwrapIndexed,
+    G.array,
+  )).rejects.toThrow('Invalid 0-based indices, didn\'t see index 0.')
+
+  await expect(G.pipe(
+    G.ofIterable([
+      { index: 0, value: 'a' },
+      { index: 1, value: 'b' },
+      { index: 3, value: 'c' }
+    ]),
+    G.unwrapIndexed,
+    G.array,
+  )).rejects.toThrow('Invalid 0-based indices, didn\'t see index 2.')
+})
---
+++ src/unwrap-indexed.ts
@@ -0,0 +1,16 @@
+/** @yields values in order based on index. */
+export async function* unwrapIndexed<T>(values: AsyncIterable<{ value: T, index: number }>): AsyncGenerator<T> {
+  const map = new Map<number, T>()
+  let j = 0
+  for await (const { index, value } of values) {
+    map.set(index, value)
+    while (map.has(j)) {
+      yield map.get(j) as T
+      map.delete(j)
+      j++
+    }
+  }
+  if (map.size) {
+    throw new Error(`Invalid 0-based indices, didn't see index ${j}.`)
+  }
+}
