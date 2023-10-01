a324388a5052ccc1b876aa6236f2817979c7356f
Mirek Rusin <mirek@me.com>
2023-10-01 11:37:09 +0200

    feat(step): throw on invalid input

--- src/step.ts
+++ src/step.ts
@@ -1,12 +1,18 @@
 /** @yields values at index where modulo is zero. */
-export function step<T>(modulo: number) {
+export function step<T>(step_: number) {
+  if (step_ < 1) {
+    throw new RangeError(`Expected step to be greater than zero, got ${step_}.`)
+  }
+  if (!Number.isSafeInteger(step_)) {
+    throw new RangeError(`Expected step to be a safe integer, got ${step_}.`)
+  }
   return async function* (values: AsyncIterable<T>): AsyncGenerator<T> {
     let i = 0
     for await (const value of values) {
       if (i === 0) {
         yield value
       }
-      if (++i === modulo) {
+      if (++i === step_) {
         i = 0
       }
     }
