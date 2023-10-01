6f6564da20c655c25d20f46d9babb7b4cba77380
Mirek Rusin <mirek@me.com>
2023-10-01 11:36:36 +0200

    fix: fixing `some`

---
+++ src/some.test.ts
@@ -0,0 +1,12 @@
+import * as G from './index.js'
+
+test('some', async () => {
+  await expect(G.pipe(
+    G.range(1, 5),
+    G.some(_ => _ > 3),
+  )).resolves.toBe(true)
+  await expect(G.pipe(
+    G.range(1, 5),
+    G.some(_ => _ > 5),
+  )).resolves.toBe(false)
+})
--- src/some.ts
+++ src/some.ts
@@ -1,8 +1,8 @@
 /** @returns `true` if at least one predicate is successful, `false` otherwise. */
 export function some<T>(predicate: (value: T, index: number) => boolean | Promise<boolean>) {
-  return async function (values: Iterable<T>) {
+  return async function (values: AsyncIterable<T>) {
     let index = 0
-    for (const value of values) {
+    for await (const value of values) {
       if (await Promise.resolve(predicate(value, index++))) {
         return true
       }
