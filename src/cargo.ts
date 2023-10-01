73b408b29a52d119f9534e5b16bf512e41575ae5
Mirek Rusin <mirek@me.com>
2023-10-01 09:15:15 +0200

    refactor: cargo

--- src/cargo.ts
+++ src/cargo.ts
@@ -4,22 +4,9 @@ import type { Transformer } from './prelude.js'
 /** @returns transformer that accumulates values since last yield; yielded batches will not exceed provided `length` (defaults to infinity). */
 export function cargo<T>(length = Infinity): Transformer<T, T[]> {
   return async function* (values) {
-    const producer = Ch.ofAsyncIterable<T>(values, length)
-    const consumer = Ch.of<T[]>()
-    while (true) {
-      const result = await producer.next()
-      if (result.done) {
-        break
-      }
-      const values = [ result.value ]
-      while (producer.pendingWrites > 0 && values.length < length) {
-        values.push(producer.consumeWrite())
-      }
-      if (consumer.done) {
-        break
-      }
-      await consumer.write(values)
+    const input = Ch.ofAsyncIterable(values, length)
+    for await (const value of input) {
+      yield [ value, ...input.consumeWrites() ]
     }
-    yield* consumer
   }
 }
